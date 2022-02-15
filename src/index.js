fc9aa49a52105dbc5d14ff41577fe2b868f807e1
Bereket Retta <bereket@etmsoftwareplc.com>
2022-02-15 20:12:57 +0300

    add some fixes on index file

--- src/index.js
+++ src/index.js
@@ -1,65 +1,156 @@
-import './style.css';
 import axios from 'axios';
-import itemsCounter from './counter.js';
-import { getMovies, GetLikes } from './request.js';
-import './contacts.js';
+import itemsCounter from './counter';
+import { getMovies, GetLikes } from './request';
+import './style.css';
+import './contacts';
 
 const main = document.querySelector('main');
 const div = document.querySelector('.movies');
+const comments = document.querySelector('.comments');
 const header = document.querySelector('.shows');
 const body = document.querySelector('body');
+const commentsDiv = document.querySelector('#comments-wrapper');
 let movies = [];
+let commentHTML = '';
+let getCommentsFromAPI = [];
 
 const displayMovie = async () => {
-  movies = getMovies();
+  movies = await getMovies();
   const likes = await GetLikes();
   const counter = itemsCounter(movies);
   header.innerHTML = 'Shows';
-  header.innerHTML += `(${counter})`;
+  header.innerHTML += ` (${counter})`;
 
   div.innerHTML = '';
   movies.forEach((movie, index) => {
     const likeVal = likes[index] !== undefined ? likes[index].likes : 0;
     div.innerHTML += `
-    <div class="movie" id="${movie.id}>
-    <img src="${movie.image.medium}" alt= ${movie.name}>
+    <div class="movie" id="${movie.id}">
+    <img src="${movie.image.medium}" alt="${movie.name}">
     <div class="likes">
     <p class="like-p">${movie.name}</p>
-    <button class="like-button"><i class="far fa-heart fa-2x" id='heart-${movie.id}'></button>
+    <button class="like-button"><i class="far fa-heart fa-2x" id='heart-${movie.id}'></i></button>
     </div>
     <p>${likeVal} likes</p>
-    <button class="comment-button">Comments</button>
-    <button class="comment-button reservations">Reservations</button>
-    </div>
-    `;
+    <button class="comment-button">Comment</button>
+    </div>`;
   });
 };
 
-const updateLikes = async (element) => {
-  await axios.post('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes', { item_id: element.id });
+const updateLikes = async (ele) => {
+  await axios.post('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/likes', { item_id: ele.id });
   displayMovie();
 };
 
-const links = document.querySelectorAll('nav li a');
-const resetLinks = () => {
-  for (let i = 0; i < links.length; i += 1) {
-    links[i].classList.remove('active');
+const popUpHtml = (target) => {
+  comments.innerHTML = `
+    <div id='pop'>
+      <div id='img-div'>
+        <i class='fas fa-times'></i>
+        <img src="${movies[parseInt(target, 10) - 1].image.medium}" alt="${movies[parseInt(target, 10) - 1].name}">
+      </div>
+      <h2>${movies[parseInt(target, 10) - 1].name}</h2>
+      <div id='comment-feature'>
+        <p> Rating: ${movies[parseInt(target, 10) - 1].rating.average}</p>
+        <p> Genres: ${movies[parseInt(target, 10) - 1].genres}</p>
+        <p> Released Date: ${movies[parseInt(target, 10) - 1].premiered}</p>
+        <p> Language: ${movies[parseInt(target, 10) - 1].language}</p>
+      </div>
+    </div>`;
+};
+
+const showComments = async (id) => {
+  try { getCommentsFromAPI = await getComments(id); } catch {
+    popUpHtml(id);
+    commentHTML = '<p>No Comments Yet</p>';
+    getCommentsFromAPI = [];
+  }
+  if (getCommentsFromAPI.length === 0) {
+    commentHTML = '<p>No Comments Yet</p>';
+  } else {
+    getCommentsFromAPI.forEach((i) => { commentHTML += `<p>${i.creation_date} ${i.username}: ${i.comment}</p>`; });
+  }
+  let commentsCount = itemsCounter(getCommentsFromAPI);
+  comments.innerHTML
+  += `<div id='comment-area'>
+        <h2>Comments (${commentsCount})</h2>
+        ${commentHTML}
+      </div>
+      <div id='comment-form'>
+        <h2>Add a Comment</h2>
+        <form action="submit" id="form-area">
+          <input type="text" id="name" placeholder="Your Name" required>
+          <textarea type="textarea" rows="4" cols="50" name="comment" placeholder="Your Insights" required></textarea>
+          <p></p>
+          <button name="${id}" type="button">Submit Comment</button>
+        </form>
+      </div>`;
+};
+
+const postNewComments = (movieID, userName, userComment) => {
+  axios.post('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments', {
+    item_id: movieID,
+    username: userName,
+    comment: userComment,
+  });
+  showComments(movieID);
+};
+
+const getComments = async (movieId) => {
+  try {
+    const comments = await axios.get(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/XMHWey4za3iNnBFD5KUq/comments?item_id=${movieId}`);
+    return comments.data;
+  } catch {
+    return [];
   }
 };
 
-body.addEventListener('click', (event) => {
-  if (event.target.classList.contains('shows')) {
+const links = document.querySelectorAll('nav li a');
+  const resetLinks = () => {
+    console.log(links);
+    for (let i = 0; i < links.length; i += 1) {
+      links[i].classList.remove('active');
+    }
+  };
+
+body.addEventListener('click', (e) => {
+  const indexID = e.target.parentNode.id;
+  if (e.target.classList.contains('shows')) {
     resetLinks();
-    event.target.classList.add('active');
-    main.innerHTML = '';
+    e.target.classList.add('active');
+    main.innerHTML='';
     main.append(div);
-  } else if (event.target.contains('fa-heart')) {
-    updateLikes(event.target);
+  }else if (e.target.className === 'comment-button') {
+    body.classList.add('fixed');
+    popUpHtml(indexID);
+    commentHTML = '';
+    showComments(indexID);
+    commentsDiv.classList.remove('d-none')
+  } else if (e.target.classList.contains('fa-heart')) {
+    updateLikes(e.target);
     displayMovie();
-    event.target.classList.add('heart');
-  } else if (event.target.classList.contains('link')) {
+    e.target.classList.add('heart');
+  } else if (e.target.innerHTML === 'Submit Comment') {
+    const sentID = (e.target.name).toString();
+    const sentUserName = (e.target.parentNode.childNodes[1].value);
+    const sentUserComment = (e.target.parentNode.childNodes[3].value);
+    if (sentUserComment && sentUserName) {
+      commentHTML = '';
+      comments.innerHTML = '';
+      popUpHtml(e.target.name);
+      postNewComments(sentID, sentUserName, sentUserComment);
+    } else {
+      const p = e.target.previousElementSibling;
+      p.innerText = 'Please insert your name and comment.';
+  
+    }
+  } else if (e.target.classList.contains('fa-times')) {
+    comments.innerHTML = '';
+    commentsDiv.classList.add('d-none');
+    body.classList.remove('fixed');
+  } else if (e.target.classList.contains('link')) {
     resetLinks();
-    event.target.classList.add('active');
+    e.target.classList.add('active');
   }
 });
 
